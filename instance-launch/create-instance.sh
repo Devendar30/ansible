aae221912bc5ac9443eed5294e81c25cc29f6bfe
Devendar30 <cheruku.devendar@gmail.com>
2021-03-19 15:52:14 +0530

    commiting

--- instance-launch/create-instance.sh
+++ instance-launch/create-instance.sh
@@ -17,7 +17,7 @@ aws ec2 run-instances --launch-template LaunchTemplateId=lt-061f7c4e2482ab086 --
 sleep 15
 fi
 
-IPADDRESS=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${component}" --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text)
+IPADDRESS=$(aws ec2 describe-instances --filters "Nam e=tag:Name,Values=${component}" --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text)
 export component
 export IPADDRESS
 
